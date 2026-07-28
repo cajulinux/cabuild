f2c298faefc936e83f1e72290028d642462e7348
stream670dark <stream@nezumu.com>
2026-07-28 14:45:09 -0300

    feat: add kernel linux recipe build script
    Signed-off-by: stream670dark <stream@nezumu.com>

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -1,4 +1,5 @@
-CAJU=/mnt/cajurootfs
+CAJU=/mnt/cajurootfs/
+TMPF=/tmp/caju/
 
 musl() {
     print "Downloading musl source..."
@@ -15,7 +16,7 @@ musl() {
     sudo make DESTDIR=$CAJU install
     print "Success musl Installed in Caju"
 
-    cd ..
+    cd $TMPF
 }
 
 mksh() {
@@ -33,7 +34,7 @@ mksh() {
 
     print "=== Success mksh Installed in Caju ===" 
 
-    cd ..
+    cd $TMPF
 }
 
 toybox() {
@@ -68,7 +69,7 @@ EOF
     done
     print "=== Toybox symlinks installed ==="
 
-    cd ..
+    cd $TMPF
 }
 
 runit() {
@@ -88,6 +89,30 @@ runit() {
     sudo cp command/* $CAJU/caju/bin/
     sudo ln -sf runit-init $CAJU/caju/bin/init
     print "=== Sucess runit Installed in Caju ==="
+
+    cd $TMPF
+}
+
+linux() {
+    print "Downloading kernel source..."
+    wget -nc https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-7.1.5.tar.xz
+    tar -xJf linux-7.1.5.tar.xz
+    cd linux-7.1.5
+
+    print "Configuring kernel..."
+    make ARCH=x86_64 LLVM=1 LLVM_IAS=1 x86_64_defconfig
+    make ARCH=x86_64 LLVM=1 LLVM_IAS=1 olddefconfig
+
+    print "Compiling kernel..."
+    KBUILD_BUILD_TIMESTAMP='' make -j$(nproc) ARCH=x86_64 LLVM=1 LLVM_IAS=1 \
+    KCFLAGS="-march=x86-64-v3" \
+    KAFLAGS="-march=x86-64-v3"
+
+    print "Installing kernel..."
+    sudo cp arch/x86/boot/bzImage /mnt/cajurootfs/efi/vmlinuz
+    print "=== Sucess Kernel Installed in Caju ==="
+
+    cd $TMPF
 }
 
 build_base() {
@@ -95,4 +120,5 @@ build_base() {
     mksh
     toybox
     runit
+    linux
 }
